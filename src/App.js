e72fcbd2237799457a6d21aad5c13b9e171f2a0b
Venera <veneraomurzakkyzy@gmail.com>
2023-12-03 22:54:44 +0600

    start doing a calculator

--- src/App.js
+++ src/App.js
@@ -2,8 +2,30 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="calculato-grid">
+      <div className="output">
+        <div className="previous-operand">
+          <div className="current-operand"></div>
+        </div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
